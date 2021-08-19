ef1483ce58db6c51dff8d06d5cd2de3804d65522
Oliver Atkinson <oatkinson@blockchain.com>
2021-08-19 10:31:11 +0100

    Use static product variants

--- Package.swift
+++ Package.swift
@@ -11,13 +11,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WalletCore",
-            url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.17/WalletCore.xcframework.zip",
-            checksum: "d723143fad427d2ed13f9b680bad470f05cfae26c02ab2379e4c987e9dcce865"
+            url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.17/static-WalletCore.xcframework.zip",
+            checksum: "e584058f54843e85a682ad58dd22b41a68a202aaac8be4eb334a9f97468915c8"
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
-            url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.17/SwiftProtobuf.xcframework.zip",
-            checksum: "5a7279c9300eb1a00677610f131e79505a18ca5ad4acddd0a13dcc6187c4b3f7"
+            url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.17/static-SwiftProtobuf.xcframework.zip",
+            checksum: "36317d1dcbadf898df225e47536ffef3b643662e59f5cb30d913225464f180eb"
         )
     ]
 )
