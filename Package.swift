44bf856ce6cea5d804c2182804e8659dc59fc82a
Oliver Atkinson <oatkinson@blockchain.com>
2021-08-23 21:17:44 +0100

    Point to new release binarys with deployment target set to 12.0

--- Package.swift
+++ Package.swift
@@ -11,13 +11,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WalletCore",
-            url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.17/static-WalletCore.xcframework.zip",
-            checksum: "e584058f54843e85a682ad58dd22b41a68a202aaac8be4eb334a9f97468915c8"
+            url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.17/WalletCore.xcframework.zip",
+            checksum: "e4fd7eb88ab7a3bef76e8451b27d1ddccaded85785446a8eae2f75eb4af2bd11"
+        ),
+        .binaryTarget(
+            name: "WalletCoreTypes",
+            url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.17/WalletCoreTypes.xcframework.zip",
+            checksum: "58e125a56514231c07c54071f1d700517b9403fdd2e55f567b00a4c39831813b"
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
-            url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.17/static-SwiftProtobuf.xcframework.zip",
-            checksum: "36317d1dcbadf898df225e47536ffef3b643662e59f5cb30d913225464f180eb"
+            url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.17/SwiftProtobuf.xcframework.zip",
+            checksum: "2e74e040886033e5470cc1efdf95d03c6c3f781f10e163861c1ffafb50fb1de7"
         )
     ]
 )
