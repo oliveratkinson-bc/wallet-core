985832e0a0746608ded3cc3984066f455d28c30f
Paulo <paulo@blockchain.com>
2021-11-01 19:11:56 +0000

    checksum

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WalletCore",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/2.6.31/WalletCore.xcframework.zip",
-            checksum: "29d88807485f88992e00a5b7ed1ddf53ca57dacb89ba5e5368dc931102600879"
+            url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.31/WalletCore.xcframework.zip",
+            checksum: "1910e722f04dea2890de1c8c3f9774506ddc1c39bcad2afb467c8c415193f8b5"
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/2.6.31/SwiftProtobuf.xcframework.zip",
-            checksum: "d1035aa8a32f2483305bbe9cd3d5774bf6c1a65ce5a37213c9ee651c78873a55"
+            url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.31/SwiftProtobuf.xcframework.zip",
+            checksum: "86312493ed0a59d16be0306c92f3584c8c6c4a0636fe5c03dd6e8e6aaf9b7121"
         )
     ]
 )
