5b4f031f62ad9acea38da1da30aa5678e001aa96
Paulo <paulo@blockchain.com>
2022-01-17 14:01:53 +0000

    Bump version

--- Package.swift
+++ Package.swift
@@ -6,22 +6,20 @@ let package = Package(
     name: "WalletCore",
     platforms: [.iOS(.v13)],
     products: [
-        .library(
-            name: "WalletCore", targets: ["WalletCore", "SwiftProtobuf"]
-        )
+        .library(name: "WalletCore", targets: ["WalletCore"]),
+        .library(name: "SwiftProtobuf", targets: ["SwiftProtobuf"])
     ],
-    dependencies: [
-    ],
+    dependencies: [],
     targets: [
         .binaryTarget(
             name: "WalletCore",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/2.6.34/WalletCore.xcframework.zip",
-            checksum: "1595ed7e1efb85e939dbbc8e7b763a8d4a4285d209f622aa960701b0cc054f0d"
+            url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.36-blockchain/WalletCore.xcframework.zip",
+            checksum: "e6c1b2bfe08923f4cfa67fe7e5e43203fe8b47fc94325948eea5c93a41130469"
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/2.6.34/SwiftProtobuf.xcframework.zip",
-            checksum: "c7839954f10427df3f47629880f1ed76c39201500c8e5efcba808e37893995b9"
+            url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.36-blockchain/SwiftProtobuf.xcframework.zip",
+            checksum: "b7f96148bba3639fe79a13f5e8905cf2e3d306de7cc3a9cf805eefaaaf2bc8ae"
         )
     ]
 )
