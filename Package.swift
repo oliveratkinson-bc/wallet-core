4c2e28758808ab01e5539b5dfca2c5ac6d5ab4bd
Oliver Atkinson <88668746+oliveratkinson-bc@users.noreply.github.com>
2022-08-29 09:46:09 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,18 +6,13 @@ let package = Package(
     name: "WalletCore",
     platforms: [.iOS(.v13)],
     products: [
-        .library(name: "WalletCore", targets: ["WalletCore", "SwiftProtobuf"])
+        .library(name: "WalletCore", targets: ["WalletCore"])
     ],
     targets: [
         .binaryTarget(
             name: "WalletCore",
             url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.17/dynamic-WalletCore.xcframework.zip",
             checksum: "b16e93c6470154bea6af2c0dfb7caac52b06b7bc538bbd91072707de5201012d"
-        ),
-        .binaryTarget(
-            name: "SwiftProtobuf",
-            url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.17/dynamic-SwiftProtobuf.xcframework.zip",
-            checksum: "5877a10a84d0650075a5addf78938def322952e8cee9277e9510641c6261b57e"
         )
     ]
 )
